5a8e1c3607b7e6328acf9469b8529411ba01a371
Chris-Mwesigwa <cmwesigwa@aetotechs.com>
2024-04-15 01:55:32 +0300

    login reload dissabled

--- src/components/Dashboard/E-commerce.tsx
+++ src/components/Dashboard/E-commerce.tsx
@@ -9,9 +9,6 @@ import CardDataStats from "../CardDataStats";
 
 const ECommerce: React.FC = () => {
 
-  useEffect(() => {
-    window.location.reload();
-  },[])
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-4 2xl:gap-7.5">
